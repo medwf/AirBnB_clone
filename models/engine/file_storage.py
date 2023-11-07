3774b06ab6e389e80b17a282f36933837f9b1cac
med.wf <med.wf95@gmail.com>
2023-11-07 11:22:38 +0100

    Fix err object should't become a dict

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,31 +3,45 @@
 import json
 from models.base_model import BaseModel
 
+"""model FileStorage"""
+
 
 class FileStorage:
+    """This file represent Class FileStorage
+    privet instance Attribute:
+        __file_path (str):  path to the JSON file (ex: file.json)
+        __objects (dict): empty but will store all objects by <class name>.id
+
+    Public instance Methodes:
+        all: returns the dictionary __objects
+        new: sets in __objects the obj with key <obj class name>.id
+        save: serializes __objects to the JSON file (path: __file_path)
+        reload:  deserializes the JSON file to __objects
+                    (only if the JSON file (__file_path)
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """returns the dictionary __objects"""
+        """Returns the dictionary __objects"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
+        """Sets in __objects the obj with key <obj class name>.id"""
         if obj:
             k = "{}.{}".format(type(obj).__name__, obj.id)
             FileStorage.__objects[k] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
-        new = FileStorage.__objects
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        new = dict(FileStorage.__objects)
         for CLS_id, obj in new.items():
             new[CLS_id] = obj.to_dict()
         with open(FileStorage.__file_path, "w") as file:
             json.dump(new, file)
 
     def reload(self):
-        """ deserializes the JSON file to __objects (only if the JSON file (__file_path)"""
+        """Deserializes the JSON file to __objects (only if the JSON file (__file_path)"""
         try:
             with open(FileStorage.__file_path, "r") as file:
                 dict_file = json.load(file)
