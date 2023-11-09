f93fe2f94da962159a0cd6fbc1694d497ad1c183
med.wf <med.wf95@gmail.com>
2023-11-09 18:23:25 +0100

    testing baseModel v2

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,27 +3,36 @@
 from models.base_model import BaseModel
 from unittest import TestCase
 from datetime import datetime
+from unittest.mock import patch
 import unittest
+import json
+import os
 
 
 class Test_BaseModel_V1(TestCase):
     """this model test class basemodel"""
 
     def test_hasattr(self):
-        # attribute exist
+        """Test attr and methods"""
         att = BaseModel()
+
+        """Test attribute exist"""
         self.assertTrue(hasattr(att, 'id'))
         self.assertTrue(hasattr(att, "created_at"))
         self.assertTrue(hasattr(att, "updated_at"))
 
-    def test_id_if_it_uuid4(self):
-        """test id"""
-        a = BaseModel()
-        # id == to uuid vesion 4
-        compare = r'^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$'
-        self.assertRegex(str(a.id), compare)
+        att.name = "test"
+        att.age = 11
+        self.assertTrue(hasattr(att, "name"))
+        self.assertTrue(hasattr(att, "age"))
 
-    def test_method_to_dict(self):
+        """Test methods exist"""
+        self.assertTrue(hasattr(att, "__init__"))
+        self.assertTrue(hasattr(att, "__str__"))
+        self.assertTrue(hasattr(att, "save"))
+        self.assertTrue(hasattr(att, "to_dict"))
+
+    def test_to_dict(self):
         """test count key"""
         Td = BaseModel()
         key = Td.to_dict().keys()
@@ -31,10 +40,10 @@ class Test_BaseModel_V1(TestCase):
             key, ['id', 'created_at', 'updated_at', '__class__'])
 
     def test_type(self):
-        # test type
+        """test type"""
         a = BaseModel()
         dic = a.to_dict()
-        # test type
+
         self.assertAlmostEqual(type(str(a)), str)
         self.assertAlmostEqual(type(a.id), str)
         self.assertAlmostEqual(type(a.created_at), datetime)
@@ -45,8 +54,8 @@ class Test_BaseModel_V1(TestCase):
         self.assertAlmostEqual(type(dic['updated_at']), str)
         self.assertAlmostEqual(type(dic['__class__']), str)
 
-    def test_iso_format_Time(self):
-        # test iso format.
+    def test_Time(self):
+        """test iso format."""
         a = BaseModel()
         time = a.to_dict()['created_at']
         cr = datetime.strptime(
@@ -60,13 +69,16 @@ class Test_BaseModel_V1(TestCase):
         self.assertEqual(time, cr.strftime(
             "%Y-%m-%dT%H:%M:%S.%f"))
 
-    def test_unique_id(self):
+    def test_unique(self):
+        """test unique"""
         a = BaseModel()
         b = BaseModel()
         # test uniaue id
         self.assertNotEqual(a.id, b.id)
+        """unique obj"""
+        self.assertNotEqual(a, b)
 
-    def test_method_save(self):
+    def test_save(self):
         a = BaseModel()
         update_befor = a.updated_at
         a.save()
@@ -74,19 +86,74 @@ class Test_BaseModel_V1(TestCase):
         # test updated time
         self.assertNotEqual(update_befor, update_after)
 
-    def test_method_str(self):
-        # test __str__:
+    def test_str(self):
+        """test __str__"""
         a = BaseModel()
         self.assertAlmostEqual(
             str(a), f"[{a.__class__.__name__}] ({a.id}) {a.__dict__}")
 
     def test_pass_argumemt(self):
+        """test pass rong argumment"""
         arg = BaseModel("test")
         with self.assertRaises(TypeError):
             arg.save("test")
 
         with self.assertRaises(TypeError):
             arg.to_dict("test")
+
+
+class Test_BaseModel_V2(TestCase):
+    """this model test class basemodel"""
+
+    def test_args(self):
+        """test pass args no change"""
+        a = BaseModel()
+        dic = a.to_dict()
+        b = BaseModel(dic)
+
+        """test object"""
+        self.assertNotEqual(a, b)
+
+        """Test unique id"""
+        self.assertNotEqual(a.id, b.id)
+
+        """test Type Time"""
+        self.assertAlmostEqual(type(b.created_at), datetime)
+        self.assertAlmostEqual(type(b.updated_at), datetime)
+
+    def test_kwargs(self):
+        """test pass Kwargs"""
+        x = BaseModel()
+        x.name = "test"
+        x.my_number = 11
+        kv = x.to_dict()
+        y = BaseModel(**kv)
+        self.assertEqual(x.id, y.id)
+        self.assertEqual(x.name, "test")
+        self.assertEqual(x.my_number, 11)
+        self.assertEqual(x.to_dict(), y.to_dict())
+
+    def test_saveV2(self):
+        """test save() Calling storage.save()"""
+        """ check if init it call: models.storage.save() """
+        with patch('models.storage.save') as mk:
+            oj = BaseModel()
+            oj.save()
+            mk.assert_called_once()
+
+    def test_json_file(self):
+        """test if it save in json file"""
+        """clear all """
+        os.remove("file.json")
+
+        """start testing"""
+        oj = BaseModel()
+        ky = f"BaseModel.{oj.id}"
+        oj.save()
+        with open("file.json", 'r') as file:
+            dic = json.load(file)
+        """ check if ky in dic"""
+        self.assertIn(ky, dic)
 
 
 if __name__ == '__main__':
